68068887eb7bf00ce1858b95db044d9f8911a625
papit-stack <linconghimire@gmail.com>
2025-08-25 17:14:15 +0545

    feat(search):search functionality for blog

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-   path('',views.home,name='home'),
    path('home/',views.home,name='home'),
+   path('',views.home,name='home'),
    path('about/',views.about,name='about'),
    path('contact/',views.contact,name='contact'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 # Blog List View
 def blog(request):
-    posts_list = BlogPost.objects.order_by('-created_at')
-    featured_post = posts_list.first() if posts_list.exists() else None
-    regular_posts = posts_list[1:5]  # next 4 posts after featured
+    query = request.GET.get('q')
+    
+    if query:
+        # Search by title or author
+        posts_list = BlogPost.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        ).order_by('-created_at')
+        featured_post = None  # No featured post for search results
+        regular_posts = None  # Show first 4 posts as “regular”
+    else:
+        posts_list = BlogPost.objects.order_by('-created_at')
+        featured_post = posts_list.first() if posts_list.exists() else None
+        regular_posts = posts_list[1:5]  # next 4 posts after featured
+
+    # Pagination
     paginator = Paginator(posts_list, 6)
     page_number = request.GET.get('page')
     posts = paginator.get_page(page_number)
+
     context = {
         'featured_post': featured_post,
         'regular_posts': regular_posts,
         'all_posts': posts,
+        'query': query,
     }
     return render(request, 'blog.html', context)
+
 
 # Blog Detail View
 def blog_detail(request, slug):
