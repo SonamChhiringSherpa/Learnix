681bd636ddaa5a88e07608fa000c9cfab4fd2636
papit-stack <linconghimire@gmail.com>
2025-08-25 16:33:28 +0545

    contact page configured

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from portfolio.models import Portfolio,Project
+from .models import ContactMessage
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
@@ -11,3 +14,14 @@ def about(request):
         'Projects': Projects
     }
     return render(request, 'about.html',context)
+
+def contact(request):
+    if request.method == 'POST':
+       name=request.POST.get('name')
+       email=request.POST.get('email')
+       subject=request.POST.get('subject')
+       message=request.POST.get('message')
+       ContactMessage.objects.create(name=name,email=email,subject=subject,message=message) 
+       messages.success(request, f"Thank you {name}, your message has been sent!")
+       return redirect("contact")    
+    return render(request, 'contact.html')
